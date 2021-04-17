bfcd68e17adf827001235c8fab9c1417fd36a892
Erich McCartney <erich.mccartney@gmail.com>
2021-04-17 08:03:18 -0400

    assignment completion

--- SDM_Assignment1_MedicalExpenses.R
+++ SDM_Assignment1_MedicalExpenses.R
@@ -15,6 +15,7 @@ which(! complete.cases(df)) #no missing values
 colSums(is.na(df)) #no missing values
 
 df1 <- df[, -c(11, 14:15, 28:29)]
+df2 <- df1[sample(1:nrow(df1),5000),]
 
 temp <- df1[, c(1:10)]
 library(PerformanceAnalytics)
@@ -31,81 +32,26 @@ chart.Correlation(temp)
 library(stargazer)
 
 m1 <- lm(medexpense ~ healthins + female + blackhisp + age*income + illnesses + ssiratio + private + 
-           married + msa, data = df1)
+           married + msa, data = df2)
 
 m2 <- lm(medexpense ~ healthins + age + female + blackhisp + logincome + illnesses +  ssiratio +private + 
-           married + msa + lowincome, data=df1)
+           married + msa + lowincome, data=df2)
 
 m3 <- lm(medexpense ~ age + female + blackhisp + logincome + illnesses +  ssiratio +private + 
-           married + msa + poverty, data=df1)
+           married + msa + poverty, data=df2)
 
 stargazer(m1, m2, m3, type='text', single.row=TRUE)
 
-m4 <- lm(adom_agentdaysonmarket ~ yrblt + privatepool + communitypool + specialsale + lppersqft + year, data=df)
-
-m5 <- lm(adom_agentdaysonmarket ~ yrblt*lppersqft + privatepool + communitypool + specialsale + year, data=df)
-
-m6 <- lm(adom_agentdaysonmarket ~ year, data=df1)
-
-stargazer(m6, m4, m5, type='text', single.row=TRUE)
-
 #' Test for assumptions
 
-plot(m2)
-plot(m5)
+plot(m1)
 
-shapiro.test(m2$res)                        # Shapiro-Wilk's test of multivariate normality
-shapiro.test(m5$res)
+shapiro.test(m1$res)                        # Shapiro-Wilk's test of multivariate normality
 
-bartlett.test(list(m2$res, m2$fit))         # Bartlett's test of homoskedasticity
-bartlett.test(list(m5$res, m5$fit))
+bartlett.test(list(m1$res, m1$fit))         # Bartlett's test of homoskedasticity
 
 library("car")                              # Multicollinearity test
 vif(m1)
-vif(m4)
 
 library(lmtest)
-dwtest(m2)                                  # Durbin-Watson test of autocorrelation
-dwtest(m5)
-
-#' Was this a good analysis? How do we know?
-#' Can we make this analysis better? How?
-
-#'------------------------------------------------------------------
-
-#' Seemingly unrelated regression (SUR) models
-#' A system of linear equations that are correlated across the equations
-#' but uncorrelated across observations. SUR corrects for correlated errors
-#' across equations
-#' 
-df <- read_excel("HuntersGreenHomeSales.xlsx", sheet='Data')
-
-eq1 <- pricesold - sqft*Beds + sqft*Beds + garages + tileroof + age +
-    privatepool + communitypool + specialsale + year
-
-eq2 <- adom_agentdaysonmarket - age*lppersqft + privatepool + communitypool + 
-  specialsale + year
-
-library(systemfit)
-system <-list(eq1=eq1, eq2=eq2)
-sur <- systemfit(system, method="SUR", date=df)
-summary (sur)
-
-summary(m2)
-summary(m5)
-
-#------------------------------------------------
-
-hist(df1$adom_agentdaysonmarket)
-hist(log(df1$adom_agentdaysonmarket))
-
-m4 <- lm(log(adom_agentdaysonmarket) ~ age + privatepool + communitypool +
-          specialsale + lppersqft + year, date=df1)
-
-m5 <- lm(log(adom_agentdaysonmarket+1) ~ age + privatepool + communitypool +
-           specialsale + lppersqft + year, date=df1)
-
-stargazer(m4, m5, type-"text", single.row = TRUE)
-
-m5$coefficients
-exp(m5$coefficients)
+dwtest(m1)                                  # Durbin-Watson test of autocorrelation
